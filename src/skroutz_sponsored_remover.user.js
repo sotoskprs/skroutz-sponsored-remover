fc30bff8e93474cecf0233e816b5f7ca8a3ddd58
sotoskprs <sotoskip@hotmail.com>
2023-12-13 16:29:38 +0200

    run script only in specific skroutz pages

--- src/skroutz_sponsored_remover.user.js
+++ src/skroutz_sponsored_remover.user.js
@@ -5,15 +5,16 @@
 // @description  Removes all sponsored products from product list (sponsored products are dublicates, so you will see them at their natural order).
 // @author       Sotiris Kipouros
 // @license      MIT
-// @match        https://www.skroutz.gr/*
+// @match        https://www.skroutz.gr/c/*
+// @match        https://www.skroutz.gr/s/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=skroutz.gr
 // @grant        none
 // @downloadURL  https://update.greasyfork.org/scripts/481045/Skroutz%20sponsored%20remover.user.js
 // @updateURL    https://update.greasyfork.org/scripts/481045/Skroutz%20sponsored%20remover.meta.js
 // ==/UserScript==
 
+// Get product list element if present
 const mainContent = document.querySelector("#sku-list")
-
 
 // Detect content changes (e.g. changing search filters) and remove again the ads
 const mutationObserver = new MutationObserver(function (mutations) {
@@ -29,6 +30,8 @@ function getAds() {
 // Removes the ads found with getAds()
 function removeAds(list) {
     // Iterate the HTMLcollection and change the opacity for all sponsores products
+
+    return false; // DEBUG - stops the ad removal
 
     [...list].map((item) => {
         // item.style.opacity = 0.2
